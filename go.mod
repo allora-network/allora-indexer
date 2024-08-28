33460f29cee9e97fe45d0621f8834e4eb509768f
Diego Campo <xmariachi@gmail.com>
2024-08-28 20:22:28 +0200

    Added options, error handling, progress download

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/aws/aws-sdk-go v1.55.5
 	github.com/jackc/pgx/v5 v5.5.5
 	github.com/rs/zerolog v1.32.0
+	github.com/schollz/progressbar/v3 v3.14.6
 	github.com/spf13/pflag v1.0.5
 )
 
@@ -16,8 +17,11 @@ require (
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/crypto v0.20.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/term v0.22.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -32,9 +32,16 @@ var awsSecretKey string
 var s3BucketName string
 var s3FileKey string
 var parallelJobs uint
-var resetDB bool
+var mode string
 
 func main() {
+	if err := run(); err != nil {
+		log.Error().Err(err).Msg("Application error")
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
@@ -46,16 +53,16 @@ func main() {
 		blocks           []string
 	)
 
-	pflag.UintVar(&workersNum, "WORKERS_NUM", 5, "Number of workers to process blocks concurrently")
-	pflag.StringVar(&nodeFlag, "NODE", "https://allora-rpc.testnet-1.testnet.allora.network/", "Node address") //# https://default-node-address:443",
+	pflag.UintVar(&workersNum, "WORKERS_NUM", 1, "Number of workers to process blocks concurrently")
+	pflag.StringVar(&nodeFlag, "NODE", "https://allora-rpc.testnet.allora.network/", "Node address") //# https://default-node-address:443",
 	pflag.StringVar(&cliAppFlag, "CLIAPP", "allorad", "CLI app to execute commands")
 	pflag.StringVar(&connectionFlag, "CONNECTION", "postgres://pump:pump@localhost:5433/pump", "Database connection string")
 	pflag.StringVar(&awsAccessKey, "AWS_ACCESS_KEY", "", "AWS access key")
 	pflag.StringVar(&awsSecretKey, "AWS_SECURITY_KEY", "", "AWS security key")
 	pflag.StringVar(&s3BucketName, "S3_BUCKET_NAME", "allora-testnet-1-indexer-backups", "AWS s3 bucket name")
-	pflag.StringVar(&s3FileKey, "S3_FILE_KEY", "pgdump-20240814-09-26-18.dump", "AWS s3 file key")
-	pflag.BoolVar(&resetDB, "RESET_DB", false, "Database reset flag")
-	pflag.UintVar(&parallelJobs, "RESTORE_PARALLEL_JOBS", 4, "Database reset flag")
+	pflag.StringVar(&s3FileKey, "S3_FILE_KEY", "filename.dump", "AWS s3 file key")
+	pflag.StringVar(&mode, "MODE", "full", "Mode: 'full' for full update, 'dump' to simply overwrite loading a dump and exit, 'empty' to create empty DB and exit")
+	pflag.UintVar(&parallelJobs, "RESTORE_PARALLEL_JOBS", 4, "Number of parallel jobs (workers) to restore the dump")
 	pflag.BoolVar(&exitWhenCaughtUp, "EXIT_APP", false, "Exit when last block is processed. If false will keep processing new blocks.")
 	pflag.Parse()
 
@@ -66,6 +73,7 @@ func main() {
 		Str("CONNECTION", connectionFlag).
 		Str("S3_BUCKET_NAME", s3BucketName).
 		Str("S3_FILE_KEY", s3FileKey).
+		Str("MODE", mode).
 		Bool("EXIT_APP", exitWhenCaughtUp).
 		Msg("pump started")
 
@@ -96,53 +104,97 @@ func main() {
 	}
 
 	// Init DB
-	initDB(connectionFlag, resetDB)
+	err := initDB(connectionFlag)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to init DB")
+		return err
+	}
 	defer closeDB()
 
-	_, err := downloadBackupFromS3()
-	if err != nil {
-		log.Log().Err(err).Msg("Failed restoring DB and start fetching blockchain data from scratch")
-		setupDB()
-	}
-
-	_ = addUniqueConstraints()
-
-	// Set up a channel to listen for interrupt signals
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-
-	// Set a cancel context to stop the workers
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// Set up a channel to listen for block heights to process
-	heightsChan := make(chan uint64, workersNum)
-	defer close(heightsChan)
-
-	wgBlocks := sync.WaitGroup{}
-	for j := uint(1); j <= workersNum; j++ {
-		wgBlocks.Add(1)
-		go worker(ctx, &wgBlocks, heightsChan)
-	}
-	defer wgBlocks.Wait() // Wait for all workers to finish at the end of the main function
-
-	if len(blocks) > 0 {
-		log.Info().Msgf("Processing only particular blocks: %v", blocks)
-		for _, block := range blocks {
-			height, err := strconv.ParseUint(block, 10, 64)
-			if err != nil {
-				log.Error().Err(err).Msgf("Failed to parse block height: %s", block)
-			}
-			heightsChan <- height
+	if mode == "empty" {
+		err := setupDB()
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to setup DB")
+			return err
 		}
-	} else {
-		// If no blocks are provided, start the main loop
-		log.Info().Msg("Starting main loop...")
-		generateBlocksLoop(ctx, signalChan, heightsChan, exitWhenCaughtUp)
-	}
+	} else if mode == "dump" {
+		_, err := restoreBackupFromS3()
+		if err != nil {
+			log.Err(err).Msg("Failed restoring DB and start fetching blockchain data from scratch")
+			return err
+		}
+	} else if mode == "full" {
+		blockInfoExists, err := tableExists("block_info")
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to check if table block_info exists")
+			return err
+		}
+		needsDownloadBackupFromS3 := false
+		if !blockInfoExists {
+			log.Info().Msg("DB is empty - restoring from S3")
+			needsDownloadBackupFromS3 = true
+		} else {
+			isDataEmpty, err := isDataEmpty("block_info")
+			if err != nil {
+				log.Error().Err(err).Msg("Failed to check if data is empty")
+				return err
+			} else if isDataEmpty {
+				log.Info().Msg("Tables exist but are empty - restoring from S3")
+				needsDownloadBackupFromS3 = true
+			}
+		}
 
-	log.Info().Msg("Exited main loop, waiting for subroutines to finish...")
-	cancel()
+		if needsDownloadBackupFromS3 {
+			log.Info().Msg("DB is empty - restoring from S3")
+			_, err := restoreBackupFromS3()
+			if err != nil {
+				log.Err(err).Msg("Failed restoring DB and start fetching blockchain data from scratch")
+				return err
+			} else {
+				log.Info().Msg("Successfully loaded dump")
+			}
+		} else {
+			log.Info().Msg("DB is not empty - skipping restore from S3")
+		}
+
+		// Set up a channel to listen for interrupt signals
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+		// Set a cancel context to stop the workers
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		// Set up a channel to listen for block heights to process
+		heightsChan := make(chan uint64, workersNum)
+		defer close(heightsChan)
+
+		wgBlocks := sync.WaitGroup{}
+		for j := uint(1); j <= workersNum; j++ {
+			wgBlocks.Add(1)
+			go worker(ctx, &wgBlocks, heightsChan)
+		}
+		defer wgBlocks.Wait() // Wait for all workers to finish at the end of the main function
+
+		if len(blocks) > 0 {
+			log.Info().Msgf("Processing only particular blocks: %v", blocks)
+			for _, block := range blocks {
+				height, err := strconv.ParseUint(block, 10, 64)
+				if err != nil {
+					log.Error().Err(err).Msgf("Failed to parse block height: %s", block)
+				}
+				heightsChan <- height
+			}
+		} else {
+			// If no blocks are provided, start the main loop
+			log.Info().Msg("Starting main loop...")
+			generateBlocksLoop(ctx, signalChan, heightsChan, exitWhenCaughtUp)
+		}
+
+		log.Info().Msg("Exited main loop, waiting for subroutines to finish...")
+		cancel()
+	}
+	return nil
 }
 
 // Generates the block heights to process in an infinite loop
