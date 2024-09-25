e4dedfdc6e1ca392db63b29ca2114287ea102a56
Diego Campo <xmariachi@gmail.com>
2024-09-25 18:21:07 +0200

    add IF EXISTS clause

--- sql-migrations/v0.5.1/202409251600_remove_topic_extra_columns.sql
+++ sql-migrations/v0.5.1/202409251600_remove_topic_extra_columns.sql
@@ -1,2 +1,2 @@
-ALTER TABLE public.topics DROP COLUMN forecast_task_score;
-ALTER TABLE public.topics DROP COLUMN reward;
+ALTER TABLE public.topics DROP COLUMN IF EXISTS forecast_task_score;
+ALTER TABLE public.topics DROP COLUMN IF EXISTS reward;
