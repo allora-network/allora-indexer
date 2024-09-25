6b8648c9a66891cd5b69464be0c42864c7ca9766
Kenny <pelusoken@gmail.com>
2024-09-25 01:57:55 -0400

    optimize migration performance due to large table size

--- sql-migrations/v0.5.1/202409241830_change_ema_scores_unique_constraint.sql
+++ sql-migrations/v0.5.1/202409241830_change_ema_scores_unique_constraint.sql
@@ -1,3 +1,8 @@
+-- Step 0: Set maintenance parameters to optimize performance
+set maintenance_work_mem = '1GB';
+set max_parallel_workers = 16; 
+set max_parallel_maintenance_workers = 8;
+
 -- Step 1: Drop the existing unique constraint
 ALTER TABLE ema_scores DROP CONSTRAINT IF EXISTS unique_ema_score_entry;
 
